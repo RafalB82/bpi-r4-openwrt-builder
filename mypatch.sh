f0d4e8a14477ba9e59f7acf8bc380f8342d0020b
Rafał Bielawski <rafal@bielaw.ski>
2026-05-08 15:06:24 +0200

    Add luci-theme-argon
    - Clone argon theme repo in mypatch.sh
    - Enable in my_final_defconfig

--- mypatch.sh
+++ mypatch.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #my_compilation
+
+# Argon theme
+if [ ! -d openwrt/package/luci-theme-argon ]; then
+    git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git openwrt/package/luci-theme-argon
+fi
+
 \cp -rf my_files/mt7988a.dtsi openwrt/target/linux/mediatek/files-6.6/arch/arm64/boot/dts/mediatek/mt7988a.dtsi
 \cp -rf my_files/mt7988a-bananapi-bpi-r4.dtsi openwrt/target/linux/mediatek/files-6.6/arch/arm64/boot/dts/mediatek/mt7988a-bananapi-bpi-r4.dtsi
 \cp -rf my_files/.config openwrt/.config
